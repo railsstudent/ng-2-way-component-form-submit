fa61522a6bb1002975786ac7ea333475f9a95e1e
Connie Leung <cleung334@gmail.com>
2023-10-03 13:58:56 +0800

    feat: include personForm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { AddressFormComponent } from './address-form/address-form.component';
+import { PersonFormComponent } from './person-form/person-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [AddressFormComponent],
+  imports: [AddressFormComponent, PersonFormComponent],
   template: `
     <div>
       <app-address-form />
+      <app-person-form />
     </div>
   `,
   styles: [`
