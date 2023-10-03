eb620731b7bc56b375fa092dd6b0b25c36f93f7a
Connie Leung <cleung334@gmail.com>
2023-10-03 13:51:52 +0800

    chore: convert to standalone app

---
+++ src/app/address-form/address-form.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-address-form',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './address-form.component.html',
+  styleUrls: ['./address-form.component.scss']
+})
+export class AddressFormComponent {
+
+}
