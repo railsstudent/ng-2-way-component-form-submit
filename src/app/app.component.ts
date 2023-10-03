cea96584b8b897f6881357fc506af220b4519d48
Connie Leung <cleung334@gmail.com>
2023-10-03 13:57:38 +0800

    chore: include addressFormComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { AddressFormComponent } from './address-form/address-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  imports: [AddressFormComponent],
+  template: `
+    <div>
+      <app-address-form />
+    </div>
+  `,
+  styles: [`
+    :host {
+      display: block;
+    }
+  `]
 })
 export class AppComponent {
   title = 'ng-component-2-way-binding-form-submit';
